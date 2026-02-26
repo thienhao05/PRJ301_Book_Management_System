774ab81d3c91d2175b55c7f0fb3f72d12c68a9a4
Thien Hao <93728241+thienhao05@users.noreply.github.com>
2026-02-26 23:00:37 +0700

    feat: :sparkles: demo login

--- src/java/utils/JPAUtil.java
+++ src/java/utils/JPAUtil.java
@@ -4,14 +4,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 public class JPAUtil {
+
     private static final EntityManagerFactory emf =
-            Persistence.createEntityManagerFactory("");
+            Persistence.createEntityManagerFactory("PRJ301_Book_Management_SystemPU");
 
     public static EntityManager getEntityManager() {
         return emf.createEntityManager();
